6abe11e3960c5fa6d9bfcd43f631f2e6cdb4667a
shaoyang liu <shaoyang liu>
2016-12-30 10:44:36 +0800

    fix export word bugs

--- src/main/java/com/wangzhixuan/service/impl/PeopleServiceImpl.java
+++ src/main/java/com/wangzhixuan/service/impl/PeopleServiceImpl.java
@@ -576,7 +576,7 @@ public class PeopleServiceImpl implements PeopleService{
 			params.put("${graduateSchool}",p.getGraduateSchool());
 			params.put("${contact}",p.getContact());
 			params.put("${relationship}",p.getRelationship());
-			params.put("${cnumber}",p.getContactNumber());
+			params.put("${number}",p.getContactNumber());
 
 			params.put("${familyInfo1Title}",p.getFamilyInfo1Title());
 			params.put("${familyInfo1Name}",p.getFamilyInfo1Name());
